729dcf2c2a63e74a0eb2a45fca512054eab1302c
Julie Zintel <JulieZintel@Julies-MacBook-Pro.local>
2016-11-10 16:48:29 +0100

    new

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+	// Mole Prefab
+	public GameObject mole;
+	public float aliveTime = 1;
+
+	// Spawn Interval
+	public int intervalMin = 4;
+	public int intervalMax = 15;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+	}
+
+	void Spawn() {
+		// Spawn the mole
+		GameObject g = (GameObject)Instantiate(mole, transform.position, Quaternion.identity);
+
+		// Make sure to destroy it after a few seconds
+		Destroy(g, aliveTime);
+
+		// Next Spawn
+		Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+	}
+
+	//----score count---------------------
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.name == "Mole") // If the fish and fisherman collide.
+		{
+			Destroy(other.gameObject); // Destroys the fish.
+			score += 1; // 1 point is added to the player's score.
+		}
+}
---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour {
+
+	public GameObject onHit;
+
+	void OnMouseDown() {
+		// Load On-Hit effect
+		GameObject g = (GameObject)Instantiate(onHit, transform.position, Quaternion.identity);
+
+		// Destroy it after a while
+		Destroy(g, 0.2f);
+	}
+}
